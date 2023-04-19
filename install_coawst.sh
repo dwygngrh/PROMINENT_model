b3a67a55c49f88dc3cb509accf51bd91ccd2e0d1
dwygngrh <75960709+dwygngrh@users.noreply.github.com>
2023-04-19 17:44:19 +0700

    Add files via upload

---
+++ install_coawst.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://code.usgs.gov/coawstmodel/COAWST.git
+
+cd COAWST
+
+mkdir Tools
+
+cd Tools
+
+git clone https://github.com/trondkr/model2roms
+
+git clone https://github.com/trondkr/ERA5-ROMS
+
+git clone https://github.com/trondkr/romstools.git
+
+
