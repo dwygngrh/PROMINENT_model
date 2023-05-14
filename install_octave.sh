ce588d803345e48311ef2ac4744e8bb168649296
dwygngrh <75960709+dwygngrh@users.noreply.github.com>
2023-05-14 20:25:02 +0700

    Update install_octave.sh

--- install_octave.sh
+++ install_octave.sh
@@ -2,23 +2,20 @@
 
 # Code buat install OCTAVE
 set +x
-#
-SRCDIR=${HOME}/library/src
-###############
-mkdir ${HOME}/library/octave
-### install dir
 INSDIR=${HOME}/library/octave
+sudo strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5
+#
 ### install octave
-cd ${SRCDIR}
-version=8.2.0
-#wget https://ftp.gnu.org/gnu/octave/octave-${version}.tar.gz
-tar -xf octave-${version}.tar.gz 
+version=5.2.0
+wget https://ftp.gnu.org/gnu/octave/octave-${version}.tar.gz
+tar -xzf octave-${version}.tar.gz
 cd octave-${version}
-./configure --prefix=${INSDIR}
+./configure --prefix=$INSDIR
 make -j 4
 make install
 echo 'export PATH='${HOME}'/library/octave/bin:$PATH' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH='${HOME}'/library/octave/lib:$LD_LIBRARY_PATH'  >> ~/.bashrc
-#
-source ~/.bashrc
-cd ..
+
+###
+echo 'logout linux dan login lagi'
+read -p "Press any key to continue... " -n1 -s
